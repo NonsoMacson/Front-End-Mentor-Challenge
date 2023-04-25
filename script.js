c3d1eda53e7e2f3e468e7506484d4dcd30d3efd1
Nwankwo, Chinonso Macson <105977827+NonsoMacson@users.noreply.github.com>
2023-04-25 14:21:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function selected () {
   const message = document.querySelector('#message')
-  message.textContent = 'submitted, Thank you'
+  message.textContent = 'Submitted, Thank you'
   setTimeout(() => {
     document.querySelector('#message').textContent = ''
   }, 3000)
